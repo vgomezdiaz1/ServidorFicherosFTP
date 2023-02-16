e1d5baa0412d6118a5713a6cad0a6113363bcfb7
dev <dev@debian>
2023-02-16 17:57:46 +0100

    Ya se guardan los ficheros

--- src/TCPCliente.java
+++ src/TCPCliente.java
@@ -7,73 +7,79 @@ import javax.crypto.*;
 public class TCPCliente {
 
     static GenerarClave keyObj;
+    static boolean claveLeida = false;
 
     public static void main(String[] args) {
         String Host = "localhost";
         int Puerto = 6000;// puerto remoto
         try {
             keyObj = recogerClave();
+            claveLeida = true;
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         }
-        Socket cliente = null;
-        PrintWriter fsalida = null;
-        ObjectInputStream reciboDelServidor = null;
-        BufferedReader in = null;
-        String cadena = "";
-        try {
-            cliente = new Socket(Host, Puerto);
-            fsalida = new PrintWriter(cliente.getOutputStream(), true);
-            reciboDelServidor = new ObjectInputStream(cliente.getInputStream());
-            in = new BufferedReader(new InputStreamReader(System.in));
-            do {
-                System.out.print("Introduce el nombre del fichero: ");
-                cadena = in.readLine();
-                fsalida.println(cadena);
-                FicheroEnvio fe = (FicheroEnvio) reciboDelServidor.readObject();
-                if (fe.getCodigo() == 200) {
-                    System.out.println(fe.getNombre() + "\t(" + fe.getLongitudFichero() + ")");
-                    byte[] resumenFicheroRecibido = fe.getContenidoFichero();
-                    byte[] ficheroDescifrado = null;
-                    try {
-                        ficheroDescifrado = descifrarBites(resumenFicheroRecibido);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    String claveFinal = obtenerHash(ficheroDescifrado);
-                    if (claveFinal.equals(fe.getFirmaFichero())) {
-                        System.out.println("Fichero correctamente recibido");        
-                        System.out.println(fe.getNombre());
-                        FileOutputStream fileOuputStream = new FileOutputStream(fe.getNombre());
-                        fileOuputStream.write(ficheroDescifrado);
-                        fileOuputStream.close();
-                    } else {
-                        System.out.println("Fichero corrupto: firma distinta a la esperada");
-                        System.out.println("Contacte con el administrador");
-                    }
-                } else if (fe.getCodigo() == 404) {
-                    System.out.println("El fichero " + fe.getNombre() + " no ha sido encontrado");
-                } else {
-                    System.out.println("Se ha producido un error");
-                }
-            } while (!cadena.trim().equals("*"));
-        } catch (Exception e) {
-            if (!cadena.equals("*")) {
-                System.out.println("Se ha producido un error");
-                System.out.println(e.getMessage());
-            }
-        } finally {
+        if (claveLeida) {
+            Socket cliente = null;
+            PrintWriter fsalida = null;
+            ObjectInputStream reciboDelServidor = null;
+            BufferedReader in = null;
+            String cadena = "";
             try {
-                fsalida.close();
-                reciboDelServidor.close();
-                in.close();
-                cliente.close();
-            } catch (IOException ex) {
-                ex.printStackTrace();
+                cliente = new Socket(Host, Puerto);
+                fsalida = new PrintWriter(cliente.getOutputStream(), true);
+                reciboDelServidor = new ObjectInputStream(cliente.getInputStream());
+                in = new BufferedReader(new InputStreamReader(System.in));
+                do {
+                    System.out.print("Introduce el nombre del fichero: ");
+                    cadena = in.readLine();
+                    fsalida.println(cadena);
+                    FicheroEnvio fe = (FicheroEnvio) reciboDelServidor.readObject();
+                    if (fe.getCodigo() == 200) {
+                        System.out.println(fe.getNombre() + "\t(" + fe.getLongitudFichero() + ")");
+                        byte[] resumenFicheroRecibido = fe.getContenidoFichero();
+                        byte[] ficheroDescifrado = null;
+                        try {
+                            ficheroDescifrado = descifrarBites(resumenFicheroRecibido);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        String claveFinal = obtenerHash(ficheroDescifrado);
+                        if (claveFinal.equals(fe.getFirmaFichero())) {
+                            System.out.println("Fichero correctamente recibido");
+                            System.out.println(fe.getNombre());
+                            FileOutputStream fileOuputStream = new FileOutputStream(fe.getNombre());
+                            fileOuputStream.write(ficheroDescifrado);
+                            fileOuputStream.close();
+                        } else {
+                            System.out.println("Fichero corrupto: firma distinta a la esperada");
+                            System.out.println("Contacte con el administrador");
+                        }
+                    } else if (fe.getCodigo() == 404) {
+                        System.out.println("El fichero " + fe.getNombre() + " no ha sido encontrado");
+                    } else {
+                        System.out.println("Se ha producido un error");
+                    }
+                } while (!cadena.trim().equals("*"));
+            } catch (Exception e) {
+                if (!cadena.equals("*")) {
+                    System.out.println("Se ha producido un error");
+                    System.out.println(e.getMessage());
+                }
+            } finally {
+                try {
+                    fsalida.close();
+                    reciboDelServidor.close();
+                    in.close();
+                    cliente.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                System.out.println("Fin de la conexión... ");
             }
-            System.out.println("Fin de la conexión... ");
+        } else {
+            System.out.println("No se ha podido acceder a la clave");
         }
     }
 
